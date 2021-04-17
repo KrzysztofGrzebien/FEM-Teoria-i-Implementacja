14f353bd42b76554c45f573003160aafd2663e14
KrzysztofGrzebien <160840@stud.prz.edu.pl>
2021-04-17 15:10:33 +0200

    Implementacja cz.1

--- main.py
+++ main.py
@@ -1,14 +1,67 @@
 import numpy as np
-import matplotlib as mp
+import matplotlib.pyplot as plt
+
+c=0
+f=0
+
+x_0=0
+x_p=1
+n=8
+
+twb_L='D'
+twb_P='D'
+
+wwb_L=0
+wwb_P=1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def generujTabliceGeometrii(x_0,x_p,n):
+    tmp1=np.arange(1,n+1,1)
+    tmp2=np.linspace(x_0,x_p,n)
 
+    ZMN=np.block([[tmp1],
+                    [tmp2]])
+    WEZLY=ZMN.transpose()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    tmp3 = np.arange(1, n, 1)
+    tmp4 = np.arange(1, n, 1)
+    tmp5 = np.arange(2, n+1, 1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    ZMN2 = np.block([[tmp3],
+                         [tmp4],
+                         [tmp5]])
+
+    ELEMENTY=ZMN2.transpose()
+    return WEZLY, ELEMENTY
+
+def rysuj_geometrie(WEZLY,ELEMENTY,WAR_BRZEGOWE):
+    x=WEZLY[:,1]
+    y=0*WEZLY[:,1]
+
+    x_pos_w=WEZLY[:,1]
+    y_pos_w=x_pos_w*0-0.01
+    text_w=WEZLY[:,0]
+
+    x_pos_e=np.zeros((len(WEZLY[:,0])-1, 1))
+    y_pos_e=np.zeros((len(WEZLY[:,0])-1, 1))
+    text_e=np.zeros((len(WEZLY[:,0])-1, 1))
+
+    for i in range(0,len(x_pos_w)):
+        plt.text(x_pos_w[i],y_pos_w[i],int(text_w[i]))
+
+    for j in range(0,len(x)-1):
+        x_pos_e[j]=(x[j]+x[j+1])/2
+        y_pos_e[j]=0.01
+        text_e[j]=ELEMENTY[j,0]
+        plt.text(x_pos_e[j], y_pos_e[j], int(text_e[j]))
+
+    plt.plot(x,y,color='green',marker='o',linestyle='dashed')
+    plt.grid(True)
+    plt.show()
+
+a,b= generujTabliceGeometrii(x_0,x_p,n)
+
+print(a)
+print(b)
+
+rysuj_geometrie(a,b,np.array([wwb_L,wwb_P]))
